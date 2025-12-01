4608e19537ac3f9f8d85a15f15a295dbb1d02b0f
Gilbert Aquino <gtagilbert08@gmail.com>
2025-12-01 15:18:39 -0500

    Initializing enum for RecipeType and Tag (Test)

---
+++ RecipeType.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class RecipeType here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+enum RecipeType
+{
+    MAIN, DESSERT, DRINK
+}
---
+++ Tag.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class Tag here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+enum Tag
+{
+    EASY, QUICK, HEALTHY, SPICY;
+}
