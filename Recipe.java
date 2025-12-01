11b425eb290062977f2c79420d7761d763f6c50d
John <jkespinosa.25@yahoo.com>
2025-12-01 15:11:07 -0500

    Initial sharing of project

---
+++ Recipe.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+/**
+ * Write a description of class Recipe here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Recipe
+{
+    String title;
+    ArrayList<Ingredients> ingrdients = new ArrayList<Ingredients>();
+
+    /**
+     * Constructor for objects of class Recipe
+     */
+    public Recipe()
+    {
+        
+    }
+}
---
+++ RecipeBook.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+/**
+ * Write a description of class RecipeBook here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class RecipeBook
+{
+    String title;
+    ArrayList<Recipe> recipe = new ArrayList<Recipe>();
+    String author;
+    String editor;
+    String creationDate;
+
+    public RecipeBook(){
+        
+    }
+    
+    public void printDetails(String title, String author, String editor, String creationDate){
+        
+    }
+    
+    public void listAll(){
+        
+    }
+    
+    public void listByType(){
+        
+    }
+    
+    public void searchByTitle(String title){
+        
+    }
+
+}
