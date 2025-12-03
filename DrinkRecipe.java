064ae39bab7bc5097552430be4e5f021b2bce132
Gilbert Aquino <onlyingamebro@gmail.com>
2025-12-03 14:38:38 -0500

    Initialize the subclasses of MainDishRecipe,DrinkRecipe and DessertRecipe

---
+++ DrinkRecipe.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class DrinkRecipe here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DrinkRecipe extends Recipe
+{
+    private boolean containsSugar = true;
+
+    /**
+     * Constructor for objects of class DrinkRecipe
+     */
+    public DrinkRecipe()
+    {
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+    }
+}
