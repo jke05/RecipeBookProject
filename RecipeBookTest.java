5ed7f1a2c7e79dca05bc84226ef9eeedf6ab6e3b
John <jkespinosa.25@yahoo.com>
2025-12-08 15:45:47 -0500

    Deleted and created new test classes

---
+++ RecipeBookTest.java
@@ -0,0 +1,35 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class RecipeBookTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+class Recipe{}
+class MainDishRecipe extends Recipe{}
+class DrinkRecipe extends Recipe{}
+class DessertRecipe extends Recipe{}
+public class RecipeBookTest
+{
+    private RecipeBook book;
+
+    @BeforeEach
+    public void setUp(){
+        book = new RecipeBook("John", "Gilbert", "Dec 8th 2025");
+        book.title = "The Book";
+    }
+    
+    @Test
+    
+
+    @AfterEach
+    public void tearDown(){
+        
+    }
+}
