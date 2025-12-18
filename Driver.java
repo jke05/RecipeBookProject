ed2740cf5d4b8c64dcaf5a4b9d45b8c8589be8d2
Gilbert <onlyingamebro@gmail.com>
2025-12-18 08:11:33 -0500

    Trying to prove, Ingredient, Unit, and scale() work together.
    toString() method not working properly because it prints the memory address and not the actual ingredient.

--- Driver.java
+++ Driver.java
@@ -16,7 +16,17 @@ public class Driver
         r1.addRating(4.0);
         book.addRecipe(r1);
         
-        System.out.println("All Recipes: ");
+        System.out.println("All Recipes:");
         book.listAll();
+        
+        Ingredient flour = new Ingredient("Flour", 1.0, Unit.CUP);
+        r1.addIngredient(flour);
+        
+        System.out.println("\nBefore scaling:");
+        r1.printRecipe();
+        
+        r1.scale(4);
+        System.out.println("After scaling:");
+        r1.printRecipe();
     }
 }
