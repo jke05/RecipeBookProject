5a5fd13d24770bf58ebd4bca50cd2c3e0d493710
Gilbert <onlyingamebro@gmail.com>
2025-12-18 08:08:16 -0500

    Adding a recipe and showing that it exists.

--- Driver.java
+++ Driver.java
@@ -8,6 +8,15 @@
 public class Driver
 {
     public static void main(String[] args) {
-        System.out.println("Recipe Book Program Started");
+        RecipeBook book = new RecipeBook("My Recipe Book", "John", "Gilbert", "Dec 2025");
+        
+        System.out.println(book);
+        
+        Recipe r1 = new Recipe("Pancakes", 2);
+        r1.addRating(4.0);
+        book.addRecipe(r1);
+        
+        System.out.println("All Recipes: ");
+        book.listAll();
     }
 }
