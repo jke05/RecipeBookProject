5f46e778b5bc38ba307715c766864d1499938697
Gilbert <onlyingamebro@gmail.com>
2025-12-18 08:03:21 -0500

    Initialization of Driver class

---
+++ Driver.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Driver here.
+ *
+ * @author Gilbert
+ * @version 1.1
+ */
+public class Driver
+{
+    public static void main(String[] args) {
+        System.out.println("Recipe Book Program Started");
+    }
+}
